3e33daf9cf6ce84bf6e01e0eb0d612042118246d
Kevin Cathaly <alakme@gmail.com>
2017-02-06 15:58:30 +0100

    tmp

---
+++ Example/Pods/Segment_Facebook_App_Events/Segment_Facebook_App_Events-umbrella.h
@@ -0,0 +1,7 @@
+#import <UIKit/UIKit.h>
+
+#import "SEGFacebookAppEventsIntegration.h"
+#import "SEGFacebookAppEventsIntegrationFactory.h"
+
+FOUNDATION_EXPORT double Segment_Facebook_App_EventsVersionNumber;
+FOUNDATION_EXPORT const unsigned char Segment_Facebook_App_EventsVersionString[];
---
+++ Example/Pods/Segment_Facebook_App_Events/Segment_Facebook_App_Events.h
@@ -0,0 +1,19 @@
+//
+//  Segment_Facebook_App_Events.h
+//  Segment_Facebook_App_Events
+//
+//  Created by Kevin CATHALY on 03/02/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Segment_Facebook_App_Events.
+FOUNDATION_EXPORT double Segment_Facebook_App_EventsVersionNumber;
+
+//! Project version string for Segment_Facebook_App_Events.
+FOUNDATION_EXPORT const unsigned char Segment_Facebook_App_EventsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Segment_Facebook_App_Events/PublicHeader.h>
+
+
