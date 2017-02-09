24aa621f22f6bf2749082a8cbfa5645ed7d10ab5
Kevin Cathaly <alakme@gmail.com>
2017-02-09 10:36:56 +0100

    ddddd

---
+++ Example/Pods/Bolts/Bolts.h
@@ -0,0 +1,19 @@
+//
+//  Bolts.h
+//  Bolts
+//
+//  Created by Kevin CATHALY on 09/02/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Bolts.
+FOUNDATION_EXPORT double BoltsVersionNumber;
+
+//! Project version string for Bolts.
+FOUNDATION_EXPORT const unsigned char BoltsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Bolts/PublicHeader.h>
+
+
---
+++ Example/Pods/FBSDKCoreKit/FBSDKCoreKit.h
@@ -0,0 +1,19 @@
+//
+//  FBSDKCoreKit.h
+//  FBSDKCoreKit
+//
+//  Created by Kevin CATHALY on 09/02/17.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FBSDKCoreKit.
+FOUNDATION_EXPORT double FBSDKCoreKitVersionNumber;
+
+//! Project version string for FBSDKCoreKit.
+FOUNDATION_EXPORT const unsigned char FBSDKCoreKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FBSDKCoreKit/PublicHeader.h>
+
+
